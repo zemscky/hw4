c9ee26d2efb4f0eef6bdf94478d445f974d93bf8
zemscky <aleksei.zemscky@yandex.ru>
2022-09-10 17:39:20 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,32 @@ public class Main {
         System.out.println("Значение переменной bananas с типом byte равно " + bananas);
         short cucumber = 20;
         System.out.println("Значение переменной cucumber с типом short равно " + cucumber);
-        long pineapple = 100;
+        long pineapple = 100L;
         System.out.println("Значение переменной pineapple с типом long равно " + pineapple);
-        float carrot = 1f;
-        System.out.println("Значение переменной carrot с типом float равно " + carrot);
-        double tomato = 1.5;
-        System.out.println("Значение переменной apple с типом double равно " + tomato);
+        float carrotWeight = 1f;
+        System.out.println("Значение переменной carrotWeight с типом float равно " + carrotWeight);
+        double tomatoWeight = 1.5;
+        System.out.println("Значение переменной tomatoWeight с типом double равно " + tomatoWeight);
         // Задача 2
         System.out.println("Задача 2 ");
+        float a = 27.12f;
+        System.out.println(a);
+        long b = 987678965549L;
+        System.out.println(b);
+        double c = 2.786;
+        System.out.println(c);
+        boolean vegetablesWeight = tomatoWeight + carrotWeight > 5;
+        System.out.println(vegetablesWeight);
+        short d = 569;
+        System.out.println(d);
+        int e = -159;
+        System.out.println(e);
+        char f = 27897;
+        System.out.println(f);
+        byte g = 67;
+        System.out.println(g);
+
+
 
     }
 }
