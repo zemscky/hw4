0e447a8aba0664c8d4f08f3740a03fb3fffc63f1
zemscky <aleksei.zemscky@yandex.ru>
2022-09-10 20:57:56 +0300

    Задача 8

--- src/Main.java
+++ src/Main.java
@@ -98,16 +98,50 @@ public class Main {
         System.out.println("При соблюдении второй диеты, спортсмену понадобится " + weightLoss2 + " дней");
         int weightLossAverage = (weightLoss1 + weightLoss2) / 2;
         System.out.println("В среднем спортсмену понадобится " + weightLossAverage + " день");
-
-
-
-
-
-
-
-
-
-
+        // Задание 8
+        System.out.println("Задание 8 ");
+        //Зарплата рабоиников
+        int employeeWages1 = 67760;
+        int employeeWages2 = 83690;
+        int employeeWages3 = 76230;
+        System.out.println("Зарплата у Маши в месяц стставляет: " + employeeWages1 + " рублей");
+        System.out.println("Зарплата у Дениса в месяц стставляет: " + employeeWages2 + " рублей");
+        System.out.println("Зарплата у Кристины в месяц стставляет: " + employeeWages3 + " рублей");
+        //Годовая зарплата
+        int yearSalary1 = employeeWages1 * 12;
+        int yearSalary2 = employeeWages2 * 12;
+        int yearSalary3 = employeeWages3 * 12;
+        System.out.println("Маша получает " + yearSalary1 + " рублей в год");
+        System.out.println("Денис получает " + yearSalary2 + " рублей в год");
+        System.out.println("Кристина получает " + yearSalary3 + " рублей в год");
+        //10% от зарплаты
+        int wagePercent1 = employeeWages1 / 10;
+        int wagePercent2 = employeeWages2 / 10;
+        int wagePercent3 = employeeWages3 / 10;
+        System.out.println("10% от зарплаты у Маши, составляет: " +wagePercent1 + " рублей в месяц");
+        System.out.println("10% от зарплаты у Дениса, составляет: " +wagePercent2 + " рублейв в месяц");
+        System.out.println("10% от зарплаты у Кристины, составляет: " +wagePercent3 + " рублейв в месяц");
+        //Новая зарплата работников
+        int employeesNewSalary1 = employeeWages1 + wagePercent1;
+        int employeesNewSalary2 = employeeWages2 + wagePercent2;
+        int employeesNewSalary3 = employeeWages3 + wagePercent3;
+        System.out.println("Зарплата с повышением у Маши составляет " + employeesNewSalary1 + " рублей");
+        System.out.println("Зарплата с повышением у Дениса составляет " + employeesNewSalary2 + " рублей");
+        System.out.println("Зарплата с повышением у Кристины составляет " + employeesNewSalary3 + " рублей");
+        //Новая годовая зарплата работников
+        int newSalaryForTheYear1 = employeesNewSalary1 * 12;
+        int newSalaryForTheYear2 = employeesNewSalary2 * 12;
+        int newSalaryForTheYear3 = employeesNewSalary3 * 12;
+        System.out.println("Годовой доход с повышением на 10% у Маши составляет " + newSalaryForTheYear1);
+        System.out.println("Годовой доход с повышением на 10% у Дениса составляет " + newSalaryForTheYear2);
+        System.out.println("Годовой доход с повышением на 10% у Кристины составляет " + newSalaryForTheYear3);
+        //Разница между годовым доходом с нынешней зарплатой и после повышения
+        int salaryDifference1 = newSalaryForTheYear1 - yearSalary1;
+        int salaryDifference2 = newSalaryForTheYear2 - yearSalary2;
+        int salaryDifference3 = newSalaryForTheYear3 - yearSalary3;
+        System.out.println("Маша теперь получает " + employeesNewSalary1 + " рублей. Годовой доход вырос на " + salaryDifference1 + " рублей");
+        System.out.println("Денис теперь получает " + employeesNewSalary2 + " рублей. Годовой доход вырос на " + salaryDifference2 + " рублей");
+        System.out.println("Крситина теперь получает " + employeesNewSalary3 + " рублей. Годовой доход вырос на " + salaryDifference3 + " рублей");
 
 
 
