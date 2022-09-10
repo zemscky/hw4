5442f7a81fcbd7dc197818b003b79a8e9f0c9ff6
zemscky <aleksei.zemscky@yandex.ru>
2022-09-10 16:56:09 +0300

    Домашняя работа 4

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача 1 ");
+        int apple = 10;
+        System.out.println("Значение переменной apple с типом int равно " + apple);
+        byte bananas = 5;
+        System.out.println("Значение переменной bananas с типом byte равно " + bananas);
+        short cucumber = 20;
+        System.out.println("Значение переменной cucumber с типом short равно " + cucumber);
+        long pineapple = 100;
+        System.out.println("Значение переменной pineapple с типом long равно " + pineapple);
+        float pepper = 1f;
+        System.out.println("Значение переменной apple с типом float равно " + apple);
+        double tomato = 1.5;
+        System.out.println("Значение переменной apple с типом double равно " + tomato);
+        // Задача 2
+        System.out.println("Задача 2 ");
+
+    }
+}
